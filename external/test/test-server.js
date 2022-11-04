02944b72743dac2288838079bbd513ddd4438a87
tmonzillo <tim.monzillo@gmail.com>
2022-11-04 16:54:47 +0000

    test updates

--- external/test/test-server.js
+++ external/test/test-server.js
@@ -27,7 +27,7 @@ describe('GET /', function () {
         if (err) {
           return done(err);
         }
-        chai.assert.isTrue(res.text.includes("<h1>Welcome to Green Dot Developers test application</h1>"));
+        chai.assert.isTrue(res.text.includes("<h1>Welcome to Green Dot Developers application</h1>"));
         return done();
       });
 
